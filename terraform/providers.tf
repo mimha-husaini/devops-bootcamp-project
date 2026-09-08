b44b71535dff22d01e340742492d9bdb01ed8bfe
mHusaini <husaini.ibrahim+devopskelas@gmail.com>
2026-09-08 22:12:15 +0800

    Revert "change key bucket to devops-project folder"
    This reverts commit 20da16f9f028bdf1b198da1f3c711c28aa479a07.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket       = "bootcamp-2026-mhusaini"
-    key          = "devops-project/terraform.tfstate"
+    key          = "terraform4/terraform.tfstate"
     region       = "ap-southeast-1"
     use_lockfile = true
   }
