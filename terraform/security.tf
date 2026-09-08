db9ce3e05ea37cdae264ef5bc2fc14c416167af1
mHusaini <husaini.ibrahim+devopskelas@gmail.com>
2026-09-08 23:19:06 +0800

    create security group private and public andd add suitable ports

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,23 +1,69 @@
-module "my_sg" {
+module "devops_public_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 6.0"
 
-  name            = "tf4-rackula-sg"
+  name            = "devops-public-sg"
   use_name_prefix = false
-  vpc_id          = module.my_vpc.vpc_id
+  vpc_id          = module.devops_vpc.vpc_id
 
   ingress_rules = {
-    http-rackula = {
+    http = {
       cidr_ipv4   = "0.0.0.0/0"
       ip_protocol = "tcp"
-      from_port   = 8080
-      to_port     = 8080
+      from_port   = 80
+      to_port     = 80
+    }
+    prometheus = {
+      cidr_ipv4   = "monitoring"
+      ip_protocol = "tcp"
+      from_port   = 9190
+      to_port     = 9190
+    }
+    ssh = {
+      cidr_ipv4   = "10.0.0.128/25"
+      ip_protocol = "tcp"
+      from_port   = 22
+      to_port     = 22
     }
   }
 
   egress_rules = {
-    all = { cidr_ipv4 = "0.0.0.0/0", ip_protocol = "-1" }
+    all = {
+      cidr_ipv4   = "0.0.0.0/0"
+      ip_protocol = "-1"
+    }
   }
 
-  tags = { Name = "tf4-rackula-sg" }
+  tags = {
+    Name = "devops-public-sg"
+  }
+}
+
+module "devops_private_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 6.0"
+
+  name            = "devops-private-sg"
+  use_name_prefix = false
+  vpc_id          = module.devops_vpc.vpc_id
+
+  ingress_rules = {
+    ssh = {
+      cidr_ipv4   = "10.0.0.128/25"
+      ip_protocol = "tcp"
+      from_port   = 22
+      to_port     = 22
+    }
+  }
+
+  egress_rules = {
+    all = {
+      cidr_ipv4   = "0.0.0.0/0"
+      ip_protocol = "-1"
+    }
+  }
+
+  tags = {
+    Name = "devops-private-sg"
+  }
 }
