45ccfdc88f2438dd522f0f97a1b2155e320815d8
mHusaini <husaini.ibrahim+devopskelas@gmail.com>
2026-09-09 23:24:18 +0800

    Auto correct by gh copilot after correcting ec2.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,9 +7,15 @@ output "ssm_command_public" {
 }
 
 output "server_ip_private" {
-  value = module.my_server_private.private_ip
+  value = {
+    for name, server in module.my_server_private :
+    name => server.private_ip
+  }
 }
 
 output "ssm_command_private" {
-  value = "aws ssm start-session --target ${module.my_server_private.id}"
+  value = {
+    for name, server in module.my_server_private :
+    name => "aws ssm start-session --target ${server.id}"
+  }
 }
