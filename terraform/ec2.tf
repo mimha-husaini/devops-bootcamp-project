8552192a0a5bf31eb0d38baec2e6818e90c4f497
mHusaini <husaini.ibrahim+devopskelas@gmail.com>
2026-09-09 23:23:47 +0800

    Buang duplicate module, consolidate under 1 module utk create 2 private server guna help gh copilot

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -20,44 +20,33 @@ module "my_server_public" {
   version = "~> 6.0"
 
   name                   = "web-server"
-  ami                    = data.aws_ami.my_ami.id
+  ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t3.micro"
   subnet_id              = module.devops_vpc.public_subnets[0]
   private_ip             = "10.0.0.5"
   create_security_group  = false
-  vpc_security_group_ids = [module.devops_sg_sg.id]
+  vpc_security_group_ids = module.devops_public_sg.id
   iam_instance_profile   = data.aws_iam_instance_profile.my_ssm_profile.name
 
   user_data = templatefile("userdata.sh", {})
- # tags      = { Name = "tf-server-public" }
+  # tags      = { Name = "tf-server-public" }
 }
 
 module "my_server_private" {
+  for_each = {
+    controller = "10.0.0.135"
+    monitoring = "10.0.0.136"
+  }
+
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "~> 6.0"
 
-  name                 = "controller"
-  ami                  = data.aws_ami.my_ami.id
+  name                 = each.key
+  ami                  = data.aws_ami.ubuntu.id
   instance_type        = "t3.micro"
   subnet_id            = module.devops_vpc.private_subnets[0]
-  private_ip           = "10.0.0.135"
+  private_ip           = each.value
   iam_instance_profile = data.aws_iam_instance_profile.my_ssm_profile.name
 
   user_data = templatefile("userdata-tunnel.sh", { tunnel_token = data.aws_ssm_parameter.token.value })
- # tags      = { Name = "tf-server-public" }
-}
-
-module "my_server_private" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 6.0"
-
-  name                 = "monitoring"
-  ami                  = data.aws_ami.my_ami.id
-  instance_type        = "t3.micro"
-  subnet_id            =  module.devops_vpc.private_subnets[0]
-  private_ip           = "10.0.0.136"
-  iam_instance_profile = data.aws_iam_instance_profile.my_ssm_profile.name
-
-  user_data = templatefile("userdata-tunnel.sh", { tunnel_token = data.aws_ssm_parameter.token.value })
- # tags      = { Name = "tf-server-public" }
 }
